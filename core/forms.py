dc20b1549c2132e58cbafacef3674a2889d6d8dc
robinson <robinpaulista85@gmail.com>
2022-05-11 13:46:56 -0300

    Corrigindo arquivos heroku

--- core/forms.py
+++ core/forms.py
@@ -6,7 +6,7 @@ from .models import Produto
 
 class ContatoForm(forms.Form):
     nome = forms.CharField(label='Nome', max_length=100)
-    email = forms.CharField(label='Email', max_length=100)
+    email = forms.EmailField(label='Email', max_length=100)
     assunto = forms.CharField(label='Assunto', max_length=120)
     mensagem = forms.CharField(label='Mensagem', widget=forms.Textarea())
 

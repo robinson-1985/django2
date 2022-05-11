3a4d19be9d93e4259a54087f72f51299cfc77a17
robinson <robinpaulista85@gmail.com>
2022-05-11 10:37:08 -0300

    Acrescentando informações

--- django2/settings.py
+++ django2/settings.py
@@ -10,11 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-from pathlib import Path, os, dj_database_url
+from pathlib import Path
+import os
+import dj_database_url
 
 #Usando PostgreSQL com Heroku
 DATABASES = {
-
+    'default': dj_database_url.config()
 }
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
